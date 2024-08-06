e6eb5073603e2fa605b511aac256010cba95fa43
megait402 <megait402@gmail.com>
2024-08-06 21:47:54 +0900

    [전진우] - tyhmeleaf 설정 ex02 추가

---
+++ src/main/java/com/jeonpeace/spring/ex/lifecycle/Ex02Controller.java
@@ -0,0 +1,16 @@
+package com.jeonpeace.spring.ex.lifecycle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Ex02Controller {
+
+	@RequestMapping("/lifecycle/ex02")
+	public String thymeleafEx() {
+		
+		return "lifecycle/ex02";
+		
+	}
+	
+}
