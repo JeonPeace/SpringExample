9f496ef9364db34e5f4844174f2b9b769c09590a
megait402 <megait402@gmail.com>
2024-08-12 21:49:29 +0900

    [전진우] - mvc ex 추가

---
+++ src/main/java/com/jeonpeace/spring/ex/mvc/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jeonpeace.spring.ex.mvc.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.jeonpeace.spring.ex.mvc.domain.User;
+
+@Mapper
+public interface UserRepository {
+
+	// 가장 최근에 등록된 행 조회
+	public User selectLastUser();
+	
+}
